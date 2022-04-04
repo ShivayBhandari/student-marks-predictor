7dd56725965bd4ba69398c8c69817ea0c3c7e83f
ShivayBhandari <54259462+ShivayBhandari@users.noreply.github.com>
2022-04-04 17:34:44 +0530

    update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 def predict():
     global df
 
@@ -39,5 +39,5 @@ def predict():
     return render_template('index.html', prediction_text='You will get [{}%] marks, when you do study [{}] hours per day '.format(output, int(features_value[0])))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True)
